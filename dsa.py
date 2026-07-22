4b1b07006e957a607ea679d8efa865c90b7af094
idrice junior <dongmojunior18@gmail.com>
2026-07-22 15:23:24 -0500

    first

--- dsa.py
+++ dsa.py
@@ -1,1 +1,27 @@
 # working on dsa and this is a great course
+
+def locate_value(cards, query):
+    position = 0
+
+    while True:
+
+        if cards[position] == query:
+            return position
+
+        position += 1 
+
+        if position == len(cards):
+            return -1
+
+test = {
+    'input':{
+        'cards': [7,9,6,5,8,1,0],
+        'query':8
+    },
+    'output':4
+}
+
+result = locate_value(*test['input'].values())
+print(result)
+print(result ==  test['output'])
+        
