e53097e51626b733ac12fa2e9a2688c1e7ad25ce
idrice junior <dongmojunior18@gmail.com>
2026-07-27 05:01:11 -0500

    first of the day

--- dsa.py
+++ dsa.py
@@ -22,6 +22,18 @@ print(result)
 print(result ==  test['output'])'''
 
 #Binary search\
+def test_location(cards, query,mid):
+    mid_number  = cards[mid]
+
+    if mid_number == query:
+        if mid-1>0 and cards[mid-1] == query:
+            return 'left'
+        else:
+            return 'found'
+    elif mid_number < query:
+        return 'right'
+    else:
+        return 'left'
 
 def locate_card(elements,query):
     start = 0
@@ -30,17 +42,18 @@ def locate_card(elements,query):
     while start <= end:
         mid = (start + end)//2
         mid_num = elements[mid]
-        print("start: "+ str(start) + " end: "+ str(end)+ " middle val: "+ str(mid_num))
-        if mid_num == query:
+        print("start: "+ str(start) + " end: "+ str(end)+ " middle val: "+ str(mid_num)+ " mid "+ str(mid))
+        results = test_location(elements, query, mid)
+        if results == 'found':
             return  mid
-        elif mid_num > query:
+        elif results == 'left':
             end = mid-1
-        else: 
+        elif results == 'right': 
             start = mid + 1
     return -1
 
 
-elt = [4,5,6,8,9,10,16,17,19,23,100]
+elt = [4,5,6,8,8,9,10,16,17,19,23,100]
 print(locate_card(elt, 23))
 
 
