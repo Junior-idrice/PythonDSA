e3103b8c363036f3a8f4828f3542d00e5e6acd27
idrice junior <dongmojunior18@gmail.com>
2026-07-26 20:26:33 -0500

    binary search complete

--- dsa.py
+++ dsa.py
@@ -1,5 +1,5 @@
 # working on dsa and this is a great course
-
+'''
 def locate_value(cards, query):
     position = 0
     while True:
@@ -19,5 +19,29 @@ test = {
 
 result = locate_value(*test['input'].values())
 print(result)
-print(result ==  test['output'])
+print(result ==  test['output'])'''
+
+#Binary search\
+
+def locate_card(elements,query):
+    start = 0
+    end = len(elements)-1
+
+    while start <= end:
+        mid = (start + end)//2
+        mid_num = elements[mid]
+        print("start: "+ str(start) + " end: "+ str(end)+ " middle val: "+ str(mid_num))
+        if mid_num == query:
+            return  mid
+        elif mid_num > query:
+            end = mid-1
+        else: 
+            start = mid + 1
+    return -1
+
+
+elt = [4,5,6,8,9,10,16,17,19,23,100]
+print(locate_card(elt, 23))
+
+
         
