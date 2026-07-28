5763146b5b427bb4394f5492dfaffc3f14174095
idrice junior <dongmojunior18@gmail.com>
2026-07-27 22:20:43 -0500

    lets go

--- binaryTress.py
+++ binaryTress.py
@@ -40,4 +40,26 @@ user2 = User('youyou','ituka','youyou18')
 user3 = User('farel','urbain','farel18')
 users = [user1,user2,user3]
 
-print(user1)
+
+class UserDatabase:
+    def __init__(self):
+            self.users = []
+    def insert(self, user):
+          i = 0
+          while i< len(self.users):
+                if self.users[i].username > user.username:
+                      break
+                i +=1
+          self.users.insert(i,user)
+
+    def find(self,username):
+        for user in self.users:
+              if user.username == username:
+                    return user
+
+    def update(self,user):
+          target = self.find(user.username)
+          target.name, target.email = user.name, user.email
+
+    def list_all(self):
+          return self.users
