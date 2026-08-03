4d82663cdfde693aadcbb4f5ef04e3fbccc1c084
idrice junior <dongmojunior18@gmail.com>
2026-08-03 17:11:46 -0500

    thi is great

---
+++ hashtablesAndDic.py
@@ -0,0 +1,30 @@
+# Here we will study hash tables
+# The ord() function in python converts a character into a number
+hashTableHeight= 4096
+data_list  = [None] * 3
+
+#print(ord('x'))
+
+def get_index(data_list, a_string):
+    result = 0
+
+    for a_character in a_string:
+        a_number = ord(a_character)
+        result += a_number
+    list_index = result % len(data_list)
+    return list_index
+
+my_list  = [None]* 48
+#print(get_index(my_list, "idrice") )
+a = ord("i")+ord("d") + ord("r")+ ord("i") + ord("c")+ ord("e")
+#print(a)
+#print(a%(len(my_list)))
+
+my_list[get_index(my_list,"idrice")] = ("idrice", 19)
+#print(my_list)
+
+print(get_index(my_list, "idrice"))
+
+index = get_index(my_list, "idrice")
+key, value = my_list[index]
+print(value)
