a2db31d81ee4f5309a5a4a30c2ea269aabddcdea
idrice junior <dongmojunior18@gmail.com>
2026-08-05 23:56:35 -0500

    this is good

---
+++ recursion_and_dynamic_programming.py
@@ -0,0 +1,14 @@
+## Longest Common Subsequence
+'''
+Question1:
+write a function to find the length of the longest common subsequence between
+two sequences. E.g Given the strings "serendipitous" and "precipitation", the 
+longest common subsequence is "reipito" and its length is 7
+
+A "sequence" is a group of items with a deterministic ordering. List,tuples and
+ranges are some common sequence types in python
+
+A "subsequence" is a sequence obtained by deleting zero or more elements from another sequence.
+For example, "edpt" is a subsequence of "serendipitous"
+#great question
+'''
