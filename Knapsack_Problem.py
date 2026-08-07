e4bc4965fa1e1608536316777bc0b2161a55431e
idrice junior <dongmojunior18@gmail.com>
2026-08-06 22:33:42 -0500

    great

--- Knapsack_Problem.py
+++ Knapsack_Problem.py
@@ -13,5 +13,14 @@ GENERAL PROBLEM STATEMENT
 Given n elements, each of which has a weight and a profit,
 determine the maximum profit that can be obtained by selecting
 a subset of the elements weighing no more than w
+
+you have the data below
+profit       [2,3,1,5,4,7]
+weight       [4,5,1,3,2,5]
+capacity     15
+
+To exhaust all the 15 capacity, we have to take the combination of weight 
+which yield the max profit, and that is weights 5,3,2 and 5
+
 '''
 
